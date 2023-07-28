8473f469033d4aeb8142d0521959783822d95d1b
rxshan <renshan.deng@gmail.com>
2023-07-28 19:17:25 +0800

    chore: add eslint configuration

---
+++ packages/eslint-config/src/index.ts
@@ -0,0 +1,80 @@
+export default {
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: '**/tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'unused-imports'],
+  settings: {
+    react: {
+      pragma: 'h',
+      fragment: 'Fragment'
+    }
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error']
+      }
+    ],
+    /* typescript rules */
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'separate-type-imports'
+      }
+    ],
+    '@typescript-eslint/consistent-type-exports': [
+      'error',
+      { fixMixedExportsWithInlineTypeSpecifier: true }
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ],
+    /* react rules */
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/no-unknown-property': 'off',
+    'react/no-string-refs': 'error',
+    'react/require-render-return': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/self-closing-comp': 'warn',
+    'react/react-in-jsx-scope': 'error',
+    'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+    /* react-hooks rules */
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
