599d40690df8e2280f009eb0eb1f1e876737b30f
rxshan <131181654+rxshan@users.noreply.github.com>
2023-07-28 14:53:58 +0000

    Deploying to gh-pages from @ wisteria-ui/wisteria-ui@e80b4de46f131f27e8b45bff9008439b4b2b1494 🚀

---
+++ _next/static/FngYNzEuDE_tWbOtJfIkS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0adf17686d81e8c6.js"],"/_error":["static/chunks/pages/_error-b8bbfe7a5c7e2afe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
